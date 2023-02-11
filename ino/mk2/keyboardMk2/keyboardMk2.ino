e34d254a5359687f8d0a3d5f40945d2c7429a696
Sam Hudock <samhudock@gmail.com>
2023-02-11 15:54:58 -0500

    Reworks firmware code and gets to first runnable state

--- ino/mk2/keyboardMk2/keyboardMk2.ino
+++ ino/mk2/keyboardMk2/keyboardMk2.ino
@@ -2,17 +2,20 @@
 #include "Keyboard.h"
 
 //Constants
+const bool SWITCH_TESTING_MODE = false;
+const bool IS_LEFT_KEYBOARD_SIDE = false;
+
 const int COLUMN_COUNT = 6;
 const int ROW_COUNT = 4;
 const int LAYER_COUNT = 2;
 
-const byte ROW_0_PIN = 8;
-const byte ROW_1_PIN = 7;
-const byte ROW_2_PIN = 6;
-const byte ROW_3_PIN = 5;
+const byte ROW_0_PIN = IS_LEFT_KEYBOARD_SIDE ? 8 : 9;
+const byte ROW_1_PIN = IS_LEFT_KEYBOARD_SIDE ? 7 : 8;
+const byte ROW_2_PIN = IS_LEFT_KEYBOARD_SIDE ? 6 : 7;
+const byte ROW_3_PIN = IS_LEFT_KEYBOARD_SIDE ? 5 : 6;
 
 const byte COL_0_PIN = A1;
-const byte COL_1_PIN = A0; 
+const byte COL_1_PIN = A0;
 const byte COL_2_PIN = 15;
 const byte COL_3_PIN = 14;
 const byte COL_4_PIN = 16;
@@ -26,175 +29,138 @@ const byte COLS[COLUMN_COUNT] = { COL_0_PIN, COL_1_PIN, COL_2_PIN, COL_3_PIN, CO
 #define KC_LAYER_MODIFIER 0x00
 #define KEY_PRINT_SCREEN 0xCE
 
+const int LEFT_SIDE_DESIGNATOR = 420;
+const int RIGHT_SIDE_DESIGNATOR = 69;
+
+const int RIGHT_LAYER_MODIFIER_KEY_ROW_INDEX = 3;
+const int RIGHT_LAYER_MODIFIER_KEY_COL_INDEX = 1;
+
+const char RIGHT_LAYER0_KEYMAP[ROW_COUNT][COLUMN_COUNT] =
+    {
+        { 'u', 'i', 'o', 'p', '[', ']' },
+        { 'j', 'k', 'l', ';', '\'', KEY_RETURN },
+        { 'm', ',', '.', '/', KEY_TAB, KEY_BACKSPACE },
+        { KEY_RIGHT_SHIFT, KC_LAYER_MODIFIER, KC_LAYER_SWAP, '(', ')', KEY_ESC }
+    };
+const char RIGHT_LAYER1_KEYMAP[ROW_COUNT][COLUMN_COUNT] =
+    {
+        { '7', '8', '9', '0', '-', '=' },
+        { KEY_DOWN_ARROW, KEY_UP_ARROW, KEY_RIGHT_ARROW, '\\', KEY_DELETE, KEY_RETURN },
+        { KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12 },
+        { KEY_RIGHT_SHIFT, KC_LAYER_MODIFIER, KC_LAYER_SWAP, KEY_PAGE_UP, KEY_PAGE_DOWN, KEY_ESC }
+    };
+
+const int LEFT_LAYER_MODIFIER_KEY_ROW_INDEX = 3;
+const int LEFT_LAYER_MODIFIER_KEY_COL_INDEX = 4;
+
+const char LEFT_LAYER0_KEYMAP[ROW_COUNT][COLUMN_COUNT] =
+    {
+        { 'q', 'w', 'e', 'r', 't', 'y' },
+        { 'a', 's', 'd', 'f', 'g', 'h' },
+        { 'z', 'x', 'c', 'v', 'b', 'n' },
+        { KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, KC_LAYER_SWAP, KC_LAYER_MODIFIER, ' ' }
+    };
+const char LEFT_LAYER1_KEYMAP[ROW_COUNT][COLUMN_COUNT] =
+    {
+        { '1', '2', '3', '4', '5', '6' },
+        { '`', KEY_PRINT_SCREEN, KEY_HOME, KEY_END, KC_NULL, KEY_LEFT_ARROW },
+        { KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6 },
+        { KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, KC_LAYER_SWAP, KC_LAYER_MODIFIER, ' ' }
+    };
+
 // Program params
 const int TESTING_SERIAL_BAUD_RATE = 115200;
 const int LOOP_DELAY_TIME = 20;
 
-// Program flags
-const bool SWITCH_TESTING_MODE = true;
-const bool IS_LEFT_KEYBOARD_SIDE = true;
-
 //Classes
-class LayerKeyLocationProvider
+class KeymapProvider
 {
     public:
-        virtual int getlayerswapkeyrow() = 0;
-        virtual int getlayerswapkeycol() = 0;
-        virtual int getlayermodifierkeyrow() = 0;
-        virtual int getlayermodifierkeycol() = 0;
-};
-
-class LayeredKeycodeProvider
-{
-    public:
-        virtual char getlayerkeycode(int layerindex, int row, int col) = 0;
-};
-
-class LeftKeymap: public LayeredKeycodeProvider, public LayerKeyLocationProvider
-{
-    private:
-        const int LAYER_SWAP_KEY_ROW_INDEX = 3;
-        const int LAYER_SWAP_KEY_COL_INDEX = 3;
-        const int LAYER_MODIFIER_KEY_ROW_INDEX = 3;
-        const int LAYER_MODIFIER_KEY_COL_INDEX = 4;
-
-        const int _layer0keymap[ROW_COUNT][COLUMN_COUNT] =
-            {
-                { 'q', 'w', 'e', 'r', 't', 'y' },
-                { 'a', 's', 'd', 'f', 'g', 'h' },
-                { 'z', 'x', 'c', 'v', 'b', 'n' },
-                { KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, KC_LAYER_SWAP, KC_LAYER_MODIFIER, ' ' }
-            };
-        const int _layer1keymap[ROW_COUNT][COLUMN_COUNT] =
-            {
-                { '1', '2', '3', '4', '5', '6' },
-                { '`', KEY_PRINT_SCREEN, KEY_HOME, KEY_END, KC_NULL, KEY_LEFT_ARROW },
-                { KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6 },
-                { KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, KC_LAYER_SWAP, KC_LAYER_MODIFIER, ' ' }
-            };
-    public:
-        char getlayerkeycode(int layerindex, int row, int col)
+        static char get_keycode(int sidedesignator, int layerindex, int row, int col)
         {
-            switch(layerindex)
+            if (LEFT_SIDE_DESIGNATOR == sidedesignator)
             {
-                case 0:
-                    _layer0keymap[row][col];
-                    break;
-                case 1:
-                    _layer1keymap[row][col];
-                    break;
+                switch(layerindex)
+                {
+                    case 0:
+                        return LEFT_LAYER0_KEYMAP[row][col];
+                    case 1:
+                        return LEFT_LAYER1_KEYMAP[row][col];
+                }
             }
+            else if (RIGHT_SIDE_DESIGNATOR == sidedesignator)
+            {
+                switch(layerindex)
+                {
+                    case 0:
+                        return RIGHT_LAYER0_KEYMAP[row][col];
+                    case 1:
+                        return RIGHT_LAYER1_KEYMAP[row][col];
+                }
+            }
+
             return KC_NULL;
         }
 
-        int getlayerswapkeyrow()
+        static int get_layer_modifier_key_row(int sidedesignator)
         {
-            return LAYER_SWAP_KEY_ROW_INDEX;
-        }
-
-        int getlayerswapkeycol()
-        {
-            return LAYER_SWAP_KEY_COL_INDEX;
-        }
-
-        int getlayermodifierkeyrow()
-        {
-            return LAYER_MODIFIER_KEY_ROW_INDEX;
-        }
-
-        int getlayermodifierkeycol()
-        {
-            return LAYER_MODIFIER_KEY_COL_INDEX;
-        }
-};
-
-class RightKeymap: public LayeredKeycodeProvider, public LayerKeyLocationProvider
-{
-    private:
-        const int LAYER_SWAP_KEY_ROW_INDEX = 3;
-        const int LAYER_SWAP_KEY_COL_INDEX = 2;
-        const int LAYER_MODIFIER_KEY_ROW_INDEX = 3;
-        const int LAYER_MODIFIER_KEY_COL_INDEX = 1;
-
-        const int _layer0keymap[ROW_COUNT][COLUMN_COUNT] =
+            switch(sidedesignator)
             {
-                { 'u', 'i', 'o', 'p', '[', ']' },
-                { 'j', 'k', 'l', ';', '\'', KEY_RETURN },
-                { 'm', ',', '.', '/', KEY_TAB, KEY_BACKSPACE },
-                { KEY_RIGHT_SHIFT, KC_LAYER_MODIFIER, KC_LAYER_SWAP, '(', ')', KEY_ESC }
-            };
-        const int _layer1keymap[ROW_COUNT][COLUMN_COUNT] =
-            {
-                { '7', '8', '9', '0', '-', '=' },
-                { KEY_DOWN_ARROW, KEY_UP_ARROW, KEY_RIGHT_ARROW, '\\', KEY_DELETE, KEY_RETURN },
-                { KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12 },
-                { KEY_RIGHT_SHIFT, KC_LAYER_MODIFIER, KC_LAYER_SWAP, KEY_PAGE_UP, KEY_PAGE_DOWN, KEY_ESC }
-            };
-
-    public:
-        char getlayerkeycode(int layerindex, int row, int col)
-        {
-            switch(layerindex)
-            {
-                case 0:
-                    _layer0keymap[row][col];
-                    break;
-                case 1:
-                    _layer1keymap[row][col];
-                    break;
+                case LEFT_SIDE_DESIGNATOR:
+                    return LEFT_LAYER_MODIFIER_KEY_ROW_INDEX;
+                case RIGHT_SIDE_DESIGNATOR:
+                    return RIGHT_LAYER_MODIFIER_KEY_ROW_INDEX;
             }
-            return KC_NULL;
+            return -1;
         }
 
-        int getlayerswapkeyrow()
+        static int get_layer_modifier_key_col(int sidedesignator)
         {
-            return LAYER_SWAP_KEY_ROW_INDEX;
+            switch(sidedesignator)
+            {
+                case LEFT_SIDE_DESIGNATOR:
+                    return LEFT_LAYER_MODIFIER_KEY_COL_INDEX;
+                case RIGHT_SIDE_DESIGNATOR:
+                    return RIGHT_LAYER_MODIFIER_KEY_COL_INDEX;
+            }
+            return -1;
         }
 
-        int getlayerswapkeycol()
-        {
-            return LAYER_SWAP_KEY_COL_INDEX;
-        }
-
-        int getlayermodifierkeyrow()
-        {
-            return LAYER_MODIFIER_KEY_ROW_INDEX;
-        }
-
-        int getlayermodifierkeycol()
-        {
-            return LAYER_MODIFIER_KEY_COL_INDEX;
-        }
+    private:
+        KeymapProvider() { }
 };
 
 //Variables
+int _sideDesignator = -1;
+
 byte _switchMatrix[ROW_COUNT][COLUMN_COUNT] = {0};
 byte _switchMatrixPrev[ROW_COUNT][COLUMN_COUNT] = {0};
-LayerKeyLocationProvider* _layerKeyProvider;
-LayeredKeycodeProvider* _keycodeProvider;
+
 int _currentlayer = 0;
-bool _isLayerModifierKeyPressed = false;
+bool _isLayerModifierKeyHeld = false;
+bool _isLayerModifierQueued = false;
 
 //Init
 void setup()
 {
     // Init serial output
     Serial.begin(TESTING_SERIAL_BAUD_RATE);
+    // Wait for serial to boot up...
+    delay(1000);
+    Serial.println("Starting keyboard...");
 
     // Init keymap
     if (IS_LEFT_KEYBOARD_SIDE)
     {
         // Use left keymap.
-        LeftKeymap lmap;
-        _layerKeyProvider = &lmap;
-        _keycodeProvider = &lmap;
+        _sideDesignator = LEFT_SIDE_DESIGNATOR;
+        Serial.println("Selecting LeftKeymap...");
     }
     else
     {
         // Use right keymap.
-        RightKeymap rmap;
-        _layerKeyProvider = &rmap;
-        _keycodeProvider = &rmap;
+        _sideDesignator = RIGHT_SIDE_DESIGNATOR;
+        Serial.println("Selecting RightKeymap...");
     }
 
     // Init default pin modes;
@@ -262,42 +228,49 @@ void set_key_states()
                 if (_switchMatrix[i][j] == 0 && _switchMatrixPrev[i][j] == 1)
                 {
                     // We started pressing a key.
-                    if (_layerKeyProvider -> getlayerswapkeyrow() == i
-                        && _layerKeyProvider -> getlayerswapkeycol() == j)
+                    if (KeymapProvider::get_layer_modifier_key_row(_sideDesignator) == i
+                        && KeymapProvider::get_layer_modifier_key_col(_sideDesignator) == j)
                     {
-                        // The layer swap key was pressed, so change layer and bail.
-                        swap_layer();
-                    }
-                    else if (_layerKeyProvider -> getlayermodifierkeyrow() == i
-                        && _layerKeyProvider -> getlayermodifierkeycol() == j)
-                    {
-                        // We've started holding down the layer modifier key.
-                        _isLayerModifierKeyPressed = true;
+                        // We've started pressing down the layer modifier key.
+                        _isLayerModifierKeyHeld = true;
+                        _isLayerModifierQueued = true;
                     }
                     else
                     {
                         // A non-layer key has been pressed.
-                        Keyboard.press(_keycodeProvider -> getlayerkeycode(_currentlayer, i, j));
+                        Keyboard.press(KeymapProvider::get_keycode(
+                            _sideDesignator,
+                            _isLayerModifierKeyHeld || _isLayerModifierQueued ? get_swapped_layer() : _currentlayer,
+                            i,
+                            j));
+
+                        // We've satisfied our queued action if it exists
+                        _isLayerModifierQueued = false;
                     }
                 }
                 else
                 {
                     // We released a key.
-                    if (_layerKeyProvider -> getlayermodifierkeyrow() == i
-                        && _layerKeyProvider -> getlayermodifierkeycol() == j)
+                    if (KeymapProvider::get_layer_modifier_key_row(_sideDesignator) == i
+                        && KeymapProvider::get_layer_modifier_key_col(_sideDesignator) == j)
                     {
                         // The layer modifier key is no longer held.
-                        _isLayerModifierKeyPressed = false;
+                        _isLayerModifierKeyHeld = false;
                     }
                     else
                     {
+                        //Keyboard.release(KeymapProvider::get_keycode(
+                        //    _sideDesignator,
+                        //    _isLayerModifierKeyHeld ? get_swapped_layer() : _currentlayer,
+                        //    i,
+                        //    j));
+
                         // A non-layer related key has been released.
                         for (int k = 0; k < LAYER_COUNT; k++)
                         {
                             // Attempt to release all keys across all layers at this location.
                             // This is to prevent bugs when swapping layers with another key held.
-                            // TODO this may need to be revised to only the current layer.
-                            Keyboard.release(_keycodeProvider -> getlayerkeycode(k, i, j));
+                            Keyboard.release(KeymapProvider::get_keycode(_sideDesignator, k, i, j));
                         }
                     }
                 }
